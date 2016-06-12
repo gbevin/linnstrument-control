1ab7aed7ad961f2ce9846425118d482511901a1b
Geert Bevin <gbevin@uwyn.com>
2016-06-12 11:15:21 +0200

    Set version to 0.9.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -31,8 +31,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "LinnStrument Control";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "0.9.0";
+    const int          versionNumber  = 0x000900;
 }
 #endif
 
